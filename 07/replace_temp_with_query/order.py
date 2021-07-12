6b6c638ffde2c9c8ee2aad92e5d3095023f60527
junehan <junehan.dev@gmail.com>
2021-07-12 18:05:48 +0900

    SUCCESS/7.4/Item
    Implemented
       1. Item class
       #. test for order instance

--- 07/replace_temp_with_query/order.py
+++ 07/replace_temp_with_query/order.py
@@ -1,11 +1,20 @@
+from item import Item
 class Order:
+	_item	: Item;
+
 	def __init__(self, q, i):
 		self._quantity = q;
 		self._item = i;
 
 	@property
 	def price(self):
-		base_price = self._quantity * self._item.price;
+		BASE_PRICE = self._quantity * self._item.price;
 		discount_factor = 0.98;
-		discount_factor = (discount_factor - 0.03) if (base_price > 1000) else discount_factor;
-		return (base_price * discount_factor);
+		discount_factor = (discount_factor - 0.03) if (BASE_PRICE > 1000) else discount_factor;
+		return (BASE_PRICE * discount_factor);
+
+if __name__ == "__main__":
+	item = Item(3000, "toy");
+	a_order = Order(30, item);
+
+	print(a_order.price);
