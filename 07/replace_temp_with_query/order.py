a341c627bb8ab6cfae508bb82e94f1ff9aff393b
junehan <junehan.dev@gmail.com>
2021-07-12 18:25:53 +0900

    ADD/7.4/Order.discount_factor.getter
    - temp to getter
       - discount_factor -> order.discounnt_factor

--- 07/replace_temp_with_query/order.py
+++ 07/replace_temp_with_query/order.py
@@ -1,22 +1,25 @@
 from item import Item
-class Order:
-	_item	: Item;
 
-	def __init__(self, q, i):
+class Order:
+	def __init__(self, q: int, i: Item):
 		self._quantity = q;
 		self._item = i;
 
 	@property
-	def base_price(self):
+	def base_price(self) -> int:
 		base_price = self._quantity * self._item.price;
 		return (base_price);
 
 	@property
-	def price(self):
-		BASE_PRICE = self.base_price;
+	def discount_factor(self) -> int:
 		discount_factor = 0.98;
-		discount_factor = (discount_factor - 0.03) if (BASE_PRICE > 1000) else discount_factor;
-		return (BASE_PRICE * discount_factor);
+		return ((discount_factor - 0.03) if (self.base_price > 1000) else discount_factor);
+
+	@property
+	def price(self) -> int:
+		BASE_PRICE = self.base_price;
+		DISCOUNT_FACTOR = self.discount_factor;
+		return (BASE_PRICE * DISCOUNT_FACTOR);
 
 if __name__ == "__main__":
 	item = Item(3000, "toy");
