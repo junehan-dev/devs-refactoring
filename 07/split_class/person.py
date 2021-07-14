cea9eb0299a86b45f1a608668f11c1a6de4e2ee8
junehan <junehan.dev@gmail.com>
2021-07-14 14:02:41 +0900

    MOVE/7.5/Person.office_number -> TelephoneNumber
    - TelephoneNumber now created with 2 arg
       1. areacode
       #. number
    - Person.office_number delegates to TelephoneNumber.number

--- 07/split_class/person.py
+++ 07/split_class/person.py
@@ -3,8 +3,7 @@ from telephone import TelephoneNumber
 class Person:
 	def __init__(self, n, o_code, o_num):
 		self._name				= n;
-		self._office_number		= o_num;
-		self._telephone_number	= TelephoneNumber(o_code);
+		self._telephone_number	= TelephoneNumber(o_code, o_num);
 
 	@property
 	def name(self):
@@ -14,26 +13,26 @@ class Person:
 		self._name = v;
 	
 	@property
+	def telephone_number(self):
+		return (self._telephone_number);
+
+	@property
 	def office_areacode(self):
 		return (self.telephone_number.areacode);
 
 	@office_areacode.setter
 	def office_areacode(self, v):
-		self.telephon_number.areacode = v;
+		self.telephone_number.areacode = v;
 
 	@property
 	def office_number(self):
-		return (self._office_number);
+		return (self.telephone_number.number);
 
 	@office_number.setter
 	def office_number(self, v):
-		self._office_number = v;
-
-	@property
-	def telephone_number(self):
-		return (self._telephone_number);
+		self.telephone_number.number = v;
 
 if	__name__ == "__main__":
 	p = Person("June han", "32A", "23-142-4423");
-	print(p.name, p.office_areacode, p.office_number, p.telephone_number);
+	print(p.name,p.telephone_number);
 
--- 07/split_class/telephone.py
+++ 07/split_class/telephone.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 @dataclass(frozen = False)
 class TelephoneNumber:
 	_areacode: str;
+	_number: str;
 
 	@property
 	def areacode(self):
@@ -12,6 +13,14 @@ class TelephoneNumber:
 	def areacode(self, v):
 		self._areacode = v;
 
+	@property
+	def number(self):
+		return (self._number);
+
+	@number.setter
+	def number(self, v):
+		self._number = v;
+
 	def __str__(self):
-		return (self.areacode + " is number");
+		return (f"{self.areacode} {self.number}");
 
