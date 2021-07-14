66c1a1ff1942be31884a6e7402ebb7109038f4df
junehan <junehan.dev@gmail.com>
2021-07-14 14:16:57 +0900

    SET_NEWINSTANCE/7.5/Person.office_areacode
    - When set office_areacode, it doesnt changes property but instance it self.

--- 07/split_class/person.py
+++ 07/split_class/person.py
@@ -8,6 +8,7 @@ class Person:
 	@property
 	def name(self):
 		return (self._name);
+
 	@name.setter
 	def name(self, v):
 		self._name = v;
@@ -16,23 +17,33 @@ class Person:
 	def telephone_number(self):
 		return (self._telephone_number);
 
+	@telephone_number.setter
+	def telephone_number(self, v: TelephoneNumber):
+		assert(isinstance(v, TelephoneNumber) == True);
+		self._telephone_number = v;
+
 	@property
 	def office_areacode(self):
 		return (self.telephone_number.areacode);
 
 	@office_areacode.setter
-	def office_areacode(self, v):
-		self.telephone_number.areacode = v;
+	def office_areacode(self, o_code):
+		num = self.telephone_number.number;
+		self._telephone_number = TelephoneNumber(o_code, num);
 
 	@property
 	def office_number(self):
 		return (self.telephone_number.number);
 
 	@office_number.setter
-	def office_number(self, v):
-		self.telephone_number.number = v;
+	def office_number(self, o_num):
+		self.telephone_number.number = o_num;
 
 if	__name__ == "__main__":
 	p = Person("June han", "32A", "23-142-4423");
 	print(p.name,p.telephone_number);
+	p.office_areacode = "42B";
+	print(p.name, p.telephone_number);
+	p.office_number = "24-523-1234";
+	print(p.name, p.telephone_number);
 
