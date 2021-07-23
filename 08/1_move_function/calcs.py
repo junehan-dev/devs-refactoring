ad3fea3ef2a5f685eaafa998a4bd05611c1d3608
junehan <junehan.dev@gmail.com>
2021-07-23 18:18:35 +0900

    MOV_FUNC/8.1/distance,radiance > caculate_distance
    - 2 functions moved into calculate_distance

--- 08/1_move_function/calcs.py
+++ 08/1_move_function/calcs.py
@@ -1,5 +1,5 @@
 import math
-
+"""
 def distance(p1, p2):
 	_EARTH_RADIUS = 3959;
 	dLat = radiance(p2[0]) - radiance(p1[0]);
@@ -14,11 +14,27 @@ def distance(p1, p2):
 
 def radiance(degrees):
 	return (degrees * math.pi / 180);
+"""
 
 def calculate_time():
 	return (600);
 
 def calculate_distance(points):
+	def distance(p1, p2):
+		_EARTH_RADIUS = 3959;
+		dLat = radiance(p2[0]) - radiance(p1[0]);
+		dLon = radiance(p2[-1]) - radiance(p1[-1]);
+		a = (math.pow(math.sin(dLat / 2), 2)
+			+ math.cos(radiance(p2[0]))
+			* math.cos(radiance(p1[0]))
+			* math.pow(math.sin(dLon / 2), 2)
+		);
+		c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a));
+		return (_EARTH_RADIUS * c);
+
+	def radiance(degrees):
+		return (degrees * math.pi / 180);
+
 	print("called global");
 	result = 0;
 	point_len = len(points);
